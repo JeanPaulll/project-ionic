5a31117928913a9502e3069ff457fe72527c6750
Jean Paul <jeanpaulwebb@gmail.com>
2019-12-04 09:36:54 -0300

    First Commit

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {SwiperOptions} from 'swiper';
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+    selector: 'app-tab2',
+    templateUrl: 'tab2.page.html',
+    styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
 
-  constructor() {}
+    config: SwiperOptions = {
+       slidesPerView: 3,
+        spaceBetween: 30,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    };
 
+    constructor() {
+    }
+
+    ionViewDidEnter() {
+    }
 }
